4400c5a61839cbf1b1e9879be12833224712f395
alesziegler <74421311+alesziegler@users.noreply.github.com>
2024-04-26 20:04:58 +0000

    Almost matrix

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 print("hello")
 
+field = []
+
+axis = list(range(9))
+field.append(axis)
+
+for i in range(9):
+  column = []
+  column.append(i)
+  field.append(column)
+
+for column in field:
+  for number in column:
+    print(number, end=" ")
+  print(end= "\n")
+  
+
+print()
+"""
+axis = list(range(9))
+
+for n in axis:
+  for n in axis:
+    print(n,end=" ")
+  print(end ="\n")
+
+print()
+
 for i in range(9):
   for j in range(9):
     print(list(range(9)),end="")
   print(end= "\n")
+"""
